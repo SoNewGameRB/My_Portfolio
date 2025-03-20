f8ff9a162e25b7f2dd45aaeeff562afad3e087c1
SonewGameRb <stupidlin11@gmail.com>
2025-03-20 16:44:43 +0800

    發布網站

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  assetsInclude: ['**/*.png', '**/*.PNG'], // 讓 Vite 正確處理 PNG 檔案
 })
